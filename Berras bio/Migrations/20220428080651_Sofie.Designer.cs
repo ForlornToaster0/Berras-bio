1100f3648cfd704cf022095f82611d72092607b9
Lady-Amazon <sofie_manninen@hotmail.com>
2022-04-28 10:50:34 +0200

    My updates

--- Berras bio/Migrations/20220428080651_Sofie.Designer.cs
+++ Berras bio/Migrations/20220428080651_Sofie.Designer.cs
@@ -4,6 +4,7 @@ using Berras_bio.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Berras_bio.Migrations
 {
     [DbContext(typeof(Berras_bioContext))]
-    partial class Berras_bioContextModelSnapshot : ModelSnapshot
+    [Migration("20220428080651_Sofie")]
+    partial class Sofie
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
