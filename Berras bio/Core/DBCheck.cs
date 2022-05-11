a16e1093b608f15687091d560360b0961e56cb99
ForlornToaster0 <samuel.ander@hotmail.com>
2022-05-11 12:35:03 +0200

    fixed my check

--- Berras bio/Core/DBCheck.cs
+++ Berras bio/Core/DBCheck.cs
@@ -4,7 +4,7 @@ namespace Berras_bio.Core;
 
 public class DBCheck
 {
-    public bool NotNegative(string title)
+    public bool NotNegative(string title, int tickets)
     {
         bool check = true;
         using (var context = new Berras_bioContext())
@@ -13,8 +13,8 @@ public class DBCheck
                 .Where(m => m.Title == title)
                 .Select(m => m.Seats)
                 .FirstOrDefault();
-
-            if (seats <= 0)
+            
+            if (seats <= 0 || seats - tickets <= 0)
             {
                 check = false;
             }
